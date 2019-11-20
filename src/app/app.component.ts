9e4aa5f7438e8fe5970a728879b23dc2cb6692b6
gunt <4261629+gunt@users.noreply.github.com>
2019-11-20 14:49:48 +0100

    update app.component HTML & .ts Typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+// app.component.ts is like the MainView in React
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'Angular-Quiz-App';
+  title = "Angular-Quiz-App";
 }
