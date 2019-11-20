47d37a53a8ea3c5c9d744ec8714cbb6c61058898
gunt <4261629+gunt@users.noreply.github.com>
2019-11-20 12:55:27 +0100

    Update questions.service.ts
    modify file called questions.service.ts (".ts" stands for ".typescript").

--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+// src/app/questions.service.ts
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class QuestionsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public getJSON(fileId: string) {
+    return this.http.get(`./assets/${fileId}.json`) as Observable<any>;
+  }
 }
