2e7296accc0c4f3bf35255327ca9060c1b312428
gunt <4261629+gunt@users.noreply.github.com>
2019-11-20 14:17:40 +0100

    Update app.module.ts ReactiveFormsModule import
    To use the Form module (the Angular module that exports everything necessary to build forms) in your component, it needs to be registered in the app.module.ts file. As discussed earlier, this is the root module for your application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { QuestionFormComponent } from './question-form/question-form.component';
+import { QuestionFormComponent } from "./question-form/question-form.component";
 
 @NgModule({
   declarations: [AppComponent, QuestionFormComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
