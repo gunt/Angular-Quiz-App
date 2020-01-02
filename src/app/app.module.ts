293543076df29e0cb143ecc80e570eac99762772
gunt <4261629+gunt@users.noreply.github.com>
2020-01-02 19:39:10 +0100

    app.component update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { ReactiveFormsModule } from "@angular/forms";
+import { MatCardModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from "./app.component";
 import { QuestionFormComponent } from "./question-form/question-form.component";
 import { ResultsComponent } from './results/results.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: "welcome", pathMatch: "prefix" },
+];
+
 @NgModule({
-  declarations: [AppComponent, QuestionFormComponent, ResultsComponent],
-  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
+  declarations: [AppComponent, QuestionFormComponent, ResultsComponent, WelcomeComponent, QuestionsComponent],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule, RouterModule.forRoot(appRoutes), BrowserAnimationsModule, MatCardModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
